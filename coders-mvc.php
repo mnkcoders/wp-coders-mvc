a8034a83dc59f1413a61919bf996cba851614177
Coder#Laptop <mnkcoders@gmail.com>
2024-06-06 19:17:55 +0200

    First Commmit

---
+++ coders-mvc.php
@@ -0,0 +1,19 @@
+<?php defined('ABSPATH') or die;
+/* * *****************************************************************************
+ * Plugin Name: Coders MVC
+ * Plugin URI: https://coderstheme.org
+ * Description: Model View Controller Framework support for Coders App Endpoints
+ * Version: 0.1
+ * Author: Coder01
+ * Author URI: 
+ * License: GPLv2 or later
+ * Text Domain: coders_mvc
+ * Domain Path: lang
+ * Class: CodersMVC
+ * 
+ * @author Coder01 <coder01@mnkcoder.com>
+ * **************************************************************************** */
+
+
+
+
